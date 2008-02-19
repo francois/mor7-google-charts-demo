93769583bdd5e6acba7bcf0e3a56e663393639bb
François Beausoleil <francois@teksol.info>
2008-02-18 21:59:12 -0500

    Initial implementation of gchart, gchart_params, gchart_debug, and example pages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,45 @@ module ApplicationHelper
     @title = title
     content_tag(:h1, h(title))
   end
+
+  def gchart_debug(data, options={})
+    content_tag(:div, h(gchart(data, options)), :class => "debug")
+  end
+
+  def gchart(data, options={})
+    params = gchart_params(data, options)
+    image_tag(
+      "http://chart.apis.google.com/chart?#{params.to_query}",
+      :size => options[:size]
+    )
+  end
+
+  def gchart_debug(data, options={})
+    params = gchart_params(data, options)
+    content_tag(:pre, "http://chart.apis.google.com/chart?\n" + params.map {|key, value| "\t#{h(key)}=#{h(value)}&"}.join("\n")[0..-2])
+  end
+
+  def gchart_params(data, options={})
+    options.reverse_merge!(:encoding => :simple, :size => "200x80")
+    returning(Hash.new) do |params|
+      params[:cht] = "lc"
+      params[:chs] = options[:size]
+      params[:chd] = send("gchart_#{options[:encoding]}_encoding", data, options[:max_value])
+    end
+  end
+
+  SIMPLE_ENCODING = [("A".."Z"), ("a".."z"), ("0".."9")].map(&:to_a).flatten
+  SIMPLE_ENCODING_LENGTH = SIMPLE_ENCODING.length.to_f
+  def gchart_simple_encoding(data, max_value=data.reject(&:blank?).max)
+    returning("s:") do |buf|
+      buf << data.map {|value|
+        value.blank? ? "_" : gchart_encode(value, max_value, SIMPLE_ENCODING)
+      }.join
+    end
+  end
+
+  def gchart_encode(value, max_value, values)
+    index = (values.length * (value / max_value.to_f)).floor
+    values[index]
+  end
 end
